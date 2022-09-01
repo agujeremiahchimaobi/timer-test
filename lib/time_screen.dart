ed0eb9c4ba5b507a8f15af304895848feecc2346
agujeremiahchimaobi <agujeremiah97@gmail.com>
2022-09-01 16:40:54 -0700

    changes

--- lib/time_screen.dart
+++ lib/time_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TimeScreen extends StatefulWidget {
@@ -8,31 +9,56 @@ class TimeScreen extends StatefulWidget {
 }
 
 class _TimeScreenState extends State<TimeScreen> {
+  final List items = [
+    '2 hours',
+    '4 hours',
+    '6 hours',
+    '8 hours',
+    '10 hours',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(30),
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'Study Today',
+            children: [
+              const Text(
+                'STUDEASY',
                 style: TextStyle(
                     color: Colors.blue,
                     fontSize: 18,
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 30.0),
-              Text('How long Do you Want To Study For?',
+              const SizedBox(height: 60.0),
+              const Text('How long Do you Want To Study For?',
                   style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                   maxLines: 3),
-              SizedBox(height: 20.0),
-              Text(
-                'I dont know what was written in thus section of the screen but just take this as it',
+              const SizedBox(height: 20.0),
+              const Text(
+                'Set your target study hours for today. you will be \nprompted at several intervals.',
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              )
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 350,
+                child: CupertinoPicker(
+                  itemExtent: 64,
+                  onSelectedItemChanged: (index) {},
+                  selectionOverlay: Container(),
+                  children: items
+                      .map(
+                        (item) => Text(
+                          item,
+                          style: const TextStyle(fontSize: 30),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
             ],
           ),
         ),
